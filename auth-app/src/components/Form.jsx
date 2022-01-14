dc46ab1b518e69bd3c5907c22f197adec075eddb
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2022-01-13 21:27:07 -0500

    Raw form fields

--- auth-app/src/components/Form.jsx
+++ auth-app/src/components/Form.jsx
@@ -1,5 +1,23 @@
 function Form() {
-  return <></>
+  return (
+    <div className="form-group ">
+    <label>Email </label>
+    <input
+      type="email"
+      className="form-control"
+      id="email"
+      placeholder="Enter email"
+    />
+    <label>Password </label>
+    <input
+      type="password"
+      className="form-control"
+      id="password"
+      placeholder="Enter Password"
+    />
+
+  </div>
+  )
 }
 
 export default Form; 
